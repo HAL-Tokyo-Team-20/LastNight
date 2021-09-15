1b267742284f849dafa9ae751da0d9db1fb195f6
katsurakanade <Kanade_PC>
2021-09-16 03:38:34 +0900

    2021_09_16 Import VFX Graph                      Add Enemy Dead Effect

--- Assets/Enemy/Script/EnemyBehavior.cs
+++ Assets/Enemy/Script/EnemyBehavior.cs
@@ -1,23 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 using DG.Tweening;
 
 public class EnemyBehavior : MonoBehaviour
 {
 
     private SpriteRenderer spriteRenderer;
+    private VisualEffect vfx;
+    private int hp = 3;
 
     // Start is called before the first frame update
     void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
+        vfx = transform.GetChild(0).GetComponent<VisualEffect>();
+        vfx.Stop();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (hp <= 0)
+        {
+            spriteRenderer.enabled = false;
+            vfx.Play();
+            StartCoroutine(DelayDestroy());
+        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -26,6 +36,13 @@ public class EnemyBehavior : MonoBehaviour
         {
             // TODO: 弾丸連続当たるとカラー戻れないバグあり
             spriteRenderer.material.DOColor(Color.red, 0.1f).SetLoops(2, LoopType.Yoyo);
+            hp--;
         }
+    }
+
+    IEnumerator DelayDestroy()
+    {
+        yield return new WaitForSeconds(1.5f);
+        Destroy(gameObject);
     }
 }
