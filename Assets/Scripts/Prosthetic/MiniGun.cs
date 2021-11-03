fa8548ae1203d60473ec699324588a50c0010286
wym <1wangyumeng@gmail.com>
2021-11-03 19:13:24 +0900

    2021_11_03 add 3 wepons

--- Assets/Scripts/Prosthetic/MiniGun.cs
+++ Assets/Scripts/Prosthetic/MiniGun.cs
@@ -7,24 +7,45 @@ public class MiniGun : Prosthetic
 {
     public GameObject bullet_object;
     SimplePlayerController playerController;
+
+    private const int ShotTimes = 6;
     public MiniGun()
     {
         this.Type = ProstheticType.MiniGun;
         playerController = GameObjectMgr.Instance.GetGameObject("Player").GetComponent<SimplePlayerController>();
 
-        // 获取预制体
-
+        // TODO: 加特林子弹的预制体
+        var handle = Addressables.LoadAssetAsync<GameObject>("Player_Bullet");
+        bullet_object = handle.WaitForCompletion();
     }
     public override void SkillActive(Vector3 offset)
     {
         //加特林攻击行为
         if (playerController.FaceRight)
         {
+            for (int i = 0; i < ShotTimes; i++)
+            {
+                CoroutineHandler.Instance.StartMyCoroutine(MyTimer.Wait(() =>
+                    {
+                        float y = Random.Range(-1.0f, 1.0f);
+                        bullet_object.GetComponent<Bullet>().Angle = y;
+                        GameObject.Instantiate(bullet_object, offset, Quaternion.Euler(0, 0, 90));
+                    }, i * 0.1f));
 
+            }
         }
         else
         {
+            for (int i = 0; i < ShotTimes; i++)
+            {
+                CoroutineHandler.Instance.StartMyCoroutine(MyTimer.Wait(() =>
+                   {
+                       float y = Random.Range(-1.0f, 1.0f);
+                       bullet_object.GetComponent<Bullet>().Angle = y + 180.0f;
+                       GameObject.Instantiate(bullet_object, offset, Quaternion.Euler(0, 0, 90));
+                   }, i * 0.1f));
 
+            }
         }
     }
 }
--- Assets/Scripts/Prosthetic/ShotGun.cs
+++ Assets/Scripts/Prosthetic/ShotGun.cs
@@ -12,18 +12,28 @@ public class ShotGun : Prosthetic
         this.Type = ProstheticType.ShotGun;
         playerController = GameObjectMgr.Instance.GetGameObject("Player").GetComponent<SimplePlayerController>();
 
-        // 获取预制体
+        // TODO: 霰弹枪子弹的预制体
+        var handle = Addressables.LoadAssetAsync<GameObject>("Player_Bullet");
+        bullet_object = handle.WaitForCompletion();
     }
     public override void SkillActive(Vector3 offset)
     {
         //霰弹枪攻击行为
         if (playerController.FaceRight)
         {
-
+            for (int i = 0; i < 5; i++)
+            {
+                bullet_object.GetComponent<Bullet>().Angle = 15.0f * i - 30.0f;
+                GameObject.Instantiate(bullet_object, offset, Quaternion.Euler(0, 0, 90));
+            }
         }
         else
         {
-
+            for (int i = 0; i < 5; i++)
+            {
+                bullet_object.GetComponent<Bullet>().Angle = 15.0f * i + 150.0f;
+                GameObject.Instantiate(bullet_object, offset, Quaternion.Euler(0, 0, 90));
+            }
         }
     }
 }
