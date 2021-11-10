011d6830a330c79a0dee2a83f9c66c11bcefa831
Kanade Katsura <katsurakanade@gmail.com>
2021-11-10 10:16:48 +0900

    2021_11_10 Fixed StrangerGen

--- Assets/Scripts/Stranger/StrangerBehavior.cs
+++ Assets/Scripts/Stranger/StrangerBehavior.cs
@@ -9,10 +9,24 @@ public class StrangerBehavior : MonoBehaviour
     public float MoveSpeed = 3.0f;
 
     private Rigidbody rb;
+    private float Player_Z;
+
+    private List<SpriteRenderer> sp_list;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        Player_Z = GameObjectMgr.Instance.GetGameObject("Player").transform.position.z;
+
+        if (MoveDir == MoveDirEnum.Right) transform.rotation = Quaternion.Euler(0, 180, 0);
+
+        MoveSpeed = Random.Range(1.0f, 2.0f);
+
+        // Get All SpriteRenderer
+        sp_list = new List<SpriteRenderer>();
+        transform.GetChild(0).GetComponentsInChildren<SpriteRenderer>(false,sp_list);
+
+        SetSortingLayer();
 
         // TODO:随机生成服饰等
 
@@ -35,6 +49,18 @@ public class StrangerBehavior : MonoBehaviour
         if (other.CompareTag("Wall"))
         {
             Destroy(gameObject);
+        }
+    }
+
+    private void SetSortingLayer()
+    {
+        string layername = "Back";
+
+        var result = Player_Z > transform.position.z? layername = "Front" : layername = "Back";
+
+        foreach(SpriteRenderer sp in sp_list)
+        {
+            sp.sortingLayerName = layername;
         }
     }
 }
--- Assets/Scripts/Stranger/StrangerGen.cs
+++ Assets/Scripts/Stranger/StrangerGen.cs
@@ -21,10 +21,14 @@ public class StrangerGen : MonoBehaviour
     [Range(-1.0f, 1.0f)]
     public float OffsetZ2;
 
+    [Tooltip("方向")]
+    public MoveDirEnum MoveDir;
+
     private float timer;
     private float genTime;
-    private bool isPlayerEnter;
+    [SerializeField]private bool isPlayerEnter;
     private Vector3 genPosition;
+
     void Start()
     {
         isPlayerEnter = false;
@@ -36,27 +40,27 @@ public class StrangerGen : MonoBehaviour
 
     void Update()
     {
-        // 玩家进入, 则直接return
-        if (isPlayerEnter)
-        {
-            return;
-        }
-
+      
         timer += Time.deltaTime;
 
         if (timer >= genTime)
         {
             timer = 0;
-            genTime = Random.Range(MinGenTime, MaxGenTime);
 
-            // 生成2个路人, 1个向左1个向右
-            Vector3 pos = genPosition + new Vector3(0, 0, OffsetZ1);
-            var obj1 = GameObject.Instantiate(StrangerObj, pos, new Quaternion(0, 0, 0, 0));
-            obj1.GetComponent<StrangerBehavior>().MoveDir = MoveDirEnum.Left;
+            if (!isPlayerEnter)
+            {
+                genTime = Random.Range(MinGenTime, MaxGenTime);
+                var genoffset = Random.Range(OffsetZ1, OffsetZ2);
+                // 11/10 Fixed
+                Vector3 pos = genPosition + new Vector3(0, 0, genoffset);
+                var obj1 = GameObject.Instantiate(StrangerObj, pos, new Quaternion(0, 0, 0, 0));
+                obj1.GetComponent<StrangerBehavior>().MoveDir = MoveDir;
+            }
+           
 
-            pos = genPosition + new Vector3(0, 0, OffsetZ2);
-            var obj2 = GameObject.Instantiate(StrangerObj, pos, new Quaternion(0, 0, 0, 0));
-            obj2.GetComponent<StrangerBehavior>().MoveDir = MoveDirEnum.Right;
+            //pos = genPosition + new Vector3(0, 0, OffsetZ2);
+            //var obj2 = GameObject.Instantiate(StrangerObj, pos, new Quaternion(0, 0, 0, 0));
+            //obj2.GetComponent<StrangerBehavior>().MoveDir = MoveDirEnum.Right;
         }
     }
 
